9420aa9499bcbe27315c7d252891da546ea98d8e
TakatoshiLee <taka.lee@mail.utoronto.ca>
2025-08-24 13:48:57 -0400

    Update: Join form link

--- components/lib/forms.ts
+++ components/lib/forms.ts
@@ -1,5 +1,5 @@
 export const FORM_URLS = {
-  join: "",
+  join: "https://l.instagram.com/?u=https%3A%2F%2Fdocs.google.com%2Fforms%2Fd%2Fe%2F1FAIpQLSe3WDeDHYxvBjzTnV-vcmY7zrc7z2sD0on-gw410rinuQTZgQ%2Fviewform%3Fusp%3Dsharing%26ouid%3D112509761681559881355%26fbclid%3DPAZXh0bgNhZW0CMTEAAacldHeS7wZpwknwhiFZrKj2j-ZSlzPLPLJWRIyvhewLZwRP5jQx2MsgQdj0jg_aem_sf9OX1_LN8gfF-qZv_Vv_A&e=AT3iji3e4H5JT4RkvJPEhVj0PL1rFtx7q72c2Nw6w1pxSZMdVshwEJxU_W11H_m2wg7VL-pmEFtSdQE6P5dCwy42w1Yrr2zAjkRuBw",
   speaker: "",
   partner: "",
   volunteer: "",
